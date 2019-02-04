6db716daf9759bf62ab80c61a59dba64f910fabb
slabach <byron.slabach@gmai.com>
2019-02-04 17:20:26 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Byron Slabach</title>
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="dist/css/style.min.css">
+</head>
+<body>
+    
+
+<script src="js/jquery-3.1.1.min.js"></script>
+<script src="js/app.js"></script>
+</body>
+</html>
